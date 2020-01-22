723d07101f658afe72933d3373c041800964c063
minimalgeek <minimalgeek@gmail.com>
2020-01-22 20:06:10 +0100

    router configured

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Link, BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <Link to="/" className="App-link">
+            Home
+          </Link>
+          <Link to="/about" className="App-link">
+            About
+          </Link>
+        </header>
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </main>
+      </BrowserRouter>
     </div>
   );
+}
+
+function Home() {
+  return <div className="App-content">Home</div>;
+}
+
+function About() {
+  return <div className="App-content">About</div>;
 }
 
 export default App;
